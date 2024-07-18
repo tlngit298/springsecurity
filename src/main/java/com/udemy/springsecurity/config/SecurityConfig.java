f1e46adb882d6fa467c9fcfe1a0879a22fe7be9a
tuanln <tuanln@vnpt.vn>
2024-07-18 23:38:17 +0700

    Add custom userDetailsService and customerPasswordEncoder

--- src/main/java/com/udemy/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/udemy/springsecurity/config/SecurityConfig.java
@@ -5,6 +5,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer.*;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -12,7 +18,6 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @Configuration
 public class SecurityConfig {
     @Bean
-    @Order(3)
     SecurityFilterChain customSecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.authorizeHttpRequests(requests -> requests
                 .requestMatchers("/contact").permitAll()
@@ -22,4 +27,17 @@ public class SecurityConfig {
         return httpSecurity.build();
     }
 
+    @Bean
+    public UserDetailsService customUserDetailsService() {
+        UserDetails user = User.withUsername("user").password("{noop}123123").authorities("user").build();
+        UserDetails admin = User.withUsername("admin")
+                .password("{MD5}b0e79dce5fefc08561c3875ae749ee3a")
+                .authorities("admin").build();
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    public PasswordEncoder customPasswordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
 }
