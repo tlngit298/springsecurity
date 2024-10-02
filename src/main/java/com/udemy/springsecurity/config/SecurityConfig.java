59e5a2095322cb41e374858f21ff97e732a5e431
tuanln <havy@MacBook-Air-cua-HA.local>
2024-10-02 20:54:38 +0700

    Add Fixing CORS

--- src/main/java/com/udemy/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/udemy/springsecurity/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.udemy.springsecurity.config;
 
 import com.udemy.springsecurity.exceptionhandling.CustomAccessDeniedHandler;
 import com.udemy.springsecurity.exceptionhandling.CustomBasicAuthenticationEntryPoint;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +13,10 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collections;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -23,7 +28,20 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain customSecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.authorizeHttpRequests(requests -> requests
+        httpSecurity
+                .cors(corsConfig -> corsConfig.configurationSource(new CorsConfigurationSource() {
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                        CorsConfiguration corsConfiguration = new CorsConfiguration();
+                        corsConfiguration.setAllowedOrigins(Collections.singletonList("*"));
+                        corsConfiguration.setAllowedMethods(Collections.singletonList("*"));
+                        corsConfiguration.setAllowCredentials(true);
+                        corsConfiguration.setAllowedHeaders(Collections.singletonList("*"));
+                        corsConfiguration.setMaxAge(3600L);
+                        return corsConfiguration;
+                    }
+                }))
+                .authorizeHttpRequests(requests -> requests
                 .requestMatchers("/contact", "/register", "/invalid-session").permitAll()
                 .anyRequest().authenticated())
                 .csrf(AbstractHttpConfigurer::disable);
